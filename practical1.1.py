db2e97a1111d3652e15732caf7aa613bd9dd850f
Priya Dodiya <priyadodiya07@gmail.com>
2026-01-20 22:35:56 +0530

    Add files via upload

---
+++ practical1.1.py
@@ -0,0 +1,13 @@
+Python 3.14.2 (tags/v3.14.2:df79316, Dec  5 2025, 17:18:21) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> a=int(input("enter first number:"))
+enter first number:20
+>>> b=int(input("enter second number:"))
+enter second number:40
+>>> if a>b:
+...     print("maximmum is",a)
+... else:
+...     print("maximum is",b)
+... 
+...     
+maximum is 40
