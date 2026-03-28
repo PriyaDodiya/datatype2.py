0bfe9c4b2334f1512a31fd088b2c167956d13161
Priya Dodiya <priyadodiya07@gmail.com>
2026-03-28 08:19:41 +0530

    Add files via upload

---
+++ write.py
@@ -0,0 +1,23 @@
+f=open("one.txt","w")
+f.write("hello students\n")
+f.write("welcome to python file handling.\n")
+f.write("learning is fun!\n")
+f.close()
+
+f=open("one.txt","w")
+f.write("new content only.\n")
+f.close()
+
+f=open("one.txt","a")
+f.write("this line is added at the end.\n")
+f.close()
+
+f=open("one.txt","w")
+lines=[
+"python programming\n"
+"file handling\n"
+"error handling\n"
+"exception handline\n"    
+]
+f.writelines(lines)
+f.close()
